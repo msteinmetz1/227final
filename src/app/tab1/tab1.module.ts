20157c60d2c97af66e80b052cc76f8de9396490a
msteinmetz1 <mrreyes@lcmail.lcsc.edu>
2019-12-11 19:22:48 -0700

    Citpt227 Final

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,13 +5,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
+import { NgCalendarModule  } from 'ionic2-calendar';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
-  ],
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+   NgCalendarModule
+],
   declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
